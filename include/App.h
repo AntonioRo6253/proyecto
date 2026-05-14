67d087186e1814bc91b4a4fb9673e15ad261a98e
Rafael <ralf524ro@gmail.com>
2026-05-14 02:00:51 -0600

    feat(app): agregar método para mostrar todos los registros y actualizar menú

--- include/App.h
+++ include/App.h
@@ -24,6 +24,7 @@ private:
     void topList();
     void anadir();
     void eliminar();
+    void mostrarTodoRegistro();
 };
 
 #endif // MENU_H
--- src/App.cpp
+++ src/App.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector> // Para std::vector
 #include "Articulo.h"
+#include <string>
 
 void App::setEntrada(int entrada)
 {
@@ -27,7 +28,8 @@ void App::iniciar()
              2. TopList
              3. añadir
              4. eliminar
-             5. Salir
+             5. Mostrar todos los registros
+             6. Salir
 ──────────────────────────────────────
 Seleccionar: )";
         entrada = solicitarNum();
@@ -45,26 +47,110 @@ Seleccionar: )";
         case 4:
             eliminar();
             break;
+        case 5:
+            mostrarTodoRegistro();
+            break;
         }
         limpiarTerminal();
     }
-    while(entrada != 5);
+    while(entrada != 6);
 }
 
 void App::buscar()
 {
-    std::string text;
+
+    std::string busqueda;
+    bool encontrado = false;
+    limpiarTerminal();
     mostrarTitulo("Buscar");
     std::cout << "\n\nBuscar: ";
+    std::cin >> busqueda;
+    std::cout << "Que tipo de registro?\n1 [Articulo]\n2 [Manual]\n";
+    int num = solicitarNum();
+    switch (num)
+    {
+    case 1:
+    {
+        if (datos_articulo.empty())
+        {
+            std::cout << "No hay Articulos.\n";
+            break;
+        }
+        for(Articulo a : datos_articulo)
+        {
+            if(busqueda == a.titulo)
+            {
+                std::cout << "Registro encontrado" << a;
+                encontrado = true;
+            }
 
-    std::cin >> text;
-    Articulo aux;
-    std::cout << aux;
+        }
+        break;
+    }
+    case 2:
+    {
+        /* FIX
+           if (datos_manual.empty())
+           {
+               std::cout << "No hay Articulos.\n";
+               break;
+           }
+           for(Manual m : datos_manual)
+           {
+               if(busqueda == m.titulo)
+               {
+                   std::cout << "Registro encontrado" << m;
+                   encontrado = true;
+               }
+
+           }*/
+        break;
+    }
+    default:
+        std::cout << "Opción no válida\n";
+    }
+    if (!encontrado) std::cout << "No se ha encontrado ninuna coincidencia\n";
+
+    system("pause");
 }
 void App::topList()
 {
     limpiarTerminal();
+    mostrarTitulo("Top List");
+    std::cout <<
+              R"(
+──────────────────────────────────────
+        Seleccione una opcion
+             1. Buscar
+             2. TopList
+             3. añadir
+             4. eliminar
+             5. Mostrar todos los registros
+             6. Salir
+──────────────────────────────────────
+Seleccionar: )";
+    int num = solicitarNum();
+    switch(num)
+    {
+    case 1:
 
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+    default:
+        std::cout << "Saliendo... .\n";
+        break;
+    }
     system("pause");
 }
 
@@ -85,7 +171,8 @@ void App::anadir()
     }
     case 2:
     {
-        Manual registro;
+        // FIX
+        // Manual registro;
         // std::cin >> registro;
         // datos_manual.push_back(registro);
         break;
@@ -158,6 +245,26 @@ void App::eliminar()
     }
     default:
         std::cout << "Opción no válida\n";
+    }
+    system("pause");
+}
+void App::mostrarTodoRegistro()
+{
+    limpiarTerminal();
+    mostrarTitulo("Articulos");
+    if (datos_articulo.empty())
+        std::cout << "No hay Articulos.\n\n";
+
+    for(size_t i = 0; i < datos_articulo.size(); i++)
+    {
+        std::cout << i + 1 << ". \n" << datos_articulo[i];
+    }
+    mostrarTitulo("Manuales");
+    if (datos_manual.empty())
+        std::cout << "No hay manuales.\n\n";
+    for(size_t i = 0; i<datos_manual.size(); i++)
+    {
+        //std::cout << i + 1 << ". \n" << datos_manual[i];
     }
     system("pause");
 }
